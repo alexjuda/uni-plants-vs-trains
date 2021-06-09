e77f77a477f16328c37f588ac3c010c5cddacd16
Alexander Juda <alexanderjuda@gmail.com>
2021-06-09 20:43:21 +0200

    Move train along bezier curve

--- train.cs
+++ train.cs
@@ -6,17 +6,22 @@ public class train : Area2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	private PathFollow2D pathFollow;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		this.pathFollow = GetNode<PathFollow2D>("/root/Node2D/Path2D/PathFollow2D");
 	}
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-	var other = GetNode<Area2D>("/root/Node2D/instrybutor");
-	GD.Print("overlaps? " + OverlapsBody(other));
+//	var pos = this.pathFollow.GetGlobalPosition();
+	var speed = 100.0f;
+	this.pathFollow.Offset = this.pathFollow.Offset + speed * delta;
+//	var pos = this.pathFollow.GetGlobalPosition();
+//	var other = GetNode<Area2D>("/root/Node2D/instrybutor");
+//	GD.Print("overlaps? " + OverlapsBody(other));
   }
 }
