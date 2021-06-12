aec241cbf36ec3cb3b6c778b655e37d174775cf3
Alexander Juda <alexanderjuda@gmail.com>
2021-06-12 18:46:16 +0200

    Add gameover screen

--- Area2D.cs
+++ Area2D.cs
@@ -3,12 +3,12 @@ using System;
 
 public class Area2D : Godot.Area2D
 {
-	private Area2D instrybutor;
+	private instrybutor instrybutor;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		instrybutor = GetNode<Area2D>("/root/Node2D/instrybutor");
+		instrybutor = GetNode<instrybutor>("/root/Node2D/instrybutor");
 	}
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +16,7 @@ public class Area2D : Godot.Area2D
   {
 	if (OverlapsArea(instrybutor)) {
 		GD.Print("overlaps");
-		
+		instrybutor.doDamage(1);
 		GetParent().QueueFree();
 	}
   }
--- instrybutor.cs
+++ instrybutor.cs
@@ -6,6 +6,7 @@ public class instrybutor : Area2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	private int hp = 3;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -17,5 +18,14 @@ public class instrybutor : Area2D
 	public override void _Process(float delta)
 	{
 //		GD.Print(GlobalPosition);
+	}
+	
+	public void doDamage(int damage) {
+		hp -= damage;
+		GD.Print(hp);
+		
+		if (hp <= 0) {
+			GetTree().ChangeScene("res://GameOverScreen.tscn");
+		}
 	}
 }
