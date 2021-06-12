dd92a357cf09ea3e1af7d8cb682691419a1aa68b
Alexander Juda <alexanderjuda@gmail.com>
2021-06-12 20:33:23 +0200

    Patryk

--- main.cs
+++ main.cs
@@ -1,17 +1,17 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class main : Node2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
+	private const int PLANT_RANGE = 100;
+	
+	private List<train> trains = new List<train>();
+	private List<plant> plants = new List<plant>();
+	private List<(bullet, train)> bullets = new List<(bullet, train)>();
 	
 	public override void _Input(InputEvent @event)
 	{
@@ -19,7 +19,8 @@ public class main : Node2D
 		if (@event is InputEventMouseButton eventMouseButton) {
 			if (eventMouseButton.Pressed) {
 				GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
-				Node2D plant = ((ResourceLoader.Load("plant.tscn") as PackedScene).Instance() as Node2D);
+				plant plant = ((ResourceLoader.Load("plant.tscn") as PackedScene).Instance() as plant);
+				plants.Add(plant);
 				plant.Position = eventMouseButton.Position;
 				AddChild(plant);
 			}
@@ -30,10 +31,50 @@ public class main : Node2D
 		// Print the size of the viewport.
 //		GD.Print("Viewport Resolution is: ", GetViewportRect().Size);
 	}
+	
+	public void registerTrain(train train) {
+		trains.Add(train);
+		GD.Print("Registered train + " + train);
+	}
+	
+	public void unregisterTrain(train train) {
+		trains.Remove(train);
+		foreach (var tuple in bullets){
+			if(tuple.Item2 == train){
+				tuple.Item1.QueueFree();
+			}
+		}
+		bullets.RemoveAll(tuple => tuple.Item2 == train);
+		GD.Print("Unegistered train + " + train);
+	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+	 foreach(var plant in plants){
+		if(!plant.CanShoot()) {
+			continue;
+		}
+		var range = plant.GetNode<Godot.Area2D>("Area2D2");
+		foreach(var train in trains){
+			var trainArea = train.GetNode<Godot.Area2D>("Area2D");
+			//GD.Print(range.Position);
+			//GD.Print(train.Position);
+			GD.Print(range.OverlapsArea(trainArea));
+			if(range.OverlapsArea(trainArea)){
+				plant.shot();
+				var bullet = ((ResourceLoader.Load("bullet.tscn") as PackedScene).Instance() as bullet);
+				bullet.Position = plant.Position;
+				AddChild(bullet);
+				bullets.Add((bullet, train));
+			}
+		}
+	}
+	
+	foreach(var bullet1 in bullets){
+		(var bullet, var train) = bullet1;
+		GD.Print(train);
+		bullet.Position += (train.Position - bullet.Position).Normalized();
+	}
+  }
 }
---
+++ plant.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class plant : Node2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	private bool canShoot = true;
+	private double timeToShoot;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	public bool CanShoot(){
+		return canShoot;
+	}
+
+	public void shot(){
+		canShoot = false;
+		timeToShoot = 2.0f;
+	}
+
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+	if(!canShoot){
+	  timeToShoot -= delta;
+		if(timeToShoot<=0){
+			canShoot = true;
+		}
+	}
+  }
+}
