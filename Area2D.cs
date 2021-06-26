dbd7c3f4290d0cae431b8908fd76bd70add4b88c
dyduch <dominik.dyduch@gmail.com>
2021-06-26 13:02:46 +0200

    add more paths to main scene

--- Area2D.cs
+++ Area2D.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Area2D : Godot.Area2D
 {
-	private instrybutor instrybutor;
+	private List<instrybutor> instrybutors = new List<instrybutor>();
 	private main main;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		instrybutor = GetNode<instrybutor>("/root/Node2D/instrybutor");
+		instrybutor instrybutor1 = GetNode<instrybutor>("/root/Node2D/instrybutor");
+		instrybutor instrybutor2 = GetNode<instrybutor>("/root/Node2D/instrybutor2");
+		instrybutor instrybutor3 = GetNode<instrybutor>("/root/Node2D/instrybutor3");
+		
+		instrybutors.Add(instrybutor1);
+		instrybutors.Add(instrybutor2);
+		instrybutors.Add(instrybutor3);
+		
+		
 		main = GetNode<main>("/root/Node2D");
 		GD.Print(main);
 	}
@@ -21,15 +30,20 @@ public class Area2D : Godot.Area2D
 		return;
 	}
 	
-	if (OverlapsArea(instrybutor)) {
-		instrybutor.doDamage(1);
-		
-		train train = GetParent() as train;
-		
-		main.unregisterTrain(train);
-		
-		return;
+	foreach(instrybutor i in instrybutors) {
+		if (OverlapsArea(i)) {
+			i.doDamage(1);
+			
+			train train = GetParent() as train;
+			
+			main.unregisterTrain(train);
+			
+			return;
+		}
 	}
+
+	
+	
 
 	foreach(plant plant1 in main.getPlants()) {
 		var plantArea = plant1.GetNode<Godot.Area2D>("Area2D");
